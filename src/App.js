5675675347b8d69142802e846632e2f71fa91fee
helen75 <71930816+helen75@users.noreply.github.com>
2022-10-22 21:48:52 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ export default function App () {
                     <Route path="/search" element = {<Search />} />
                     <Route path="/contact" element = {<Contact />} /> */}
                 </Routes> 
-                    adfksjdfmd
+                   helşnnnn
                 {/* <Footer /> */}
             </Router>
         </>
